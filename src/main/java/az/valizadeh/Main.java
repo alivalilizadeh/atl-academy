6587b66f03f0e0b1ca51a1783421402bd2a0715b
alivalilizadeh <alivaliyef08@gmail.com>
2023-03-09 19:24:54 +0400

    initial repo

--- src/main/java/az/valizadeh/Main.java
+++ src/main/java/az/valizadeh/Main.java
@@ -1,7 +1,16 @@
 package az.valizadeh;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+
+
     }
 }
+
+
+
